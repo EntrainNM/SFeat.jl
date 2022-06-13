7643a6d102c3fcf3bf12efa846fe192993ae1e6a
Documenter.jl <documenter@juliadocs.github.io>
2022-06-13 20:26:48 +0000

    build based on beb4c2e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SFeat.jl","page":"SFeat.jl","title":"SFeat.jl","text":"","category":"section"},{"location":"","page":"SFeat.jl","title":"SFeat.jl","text":"Documentation for SFeat.jl","category":"page"}]
+}
